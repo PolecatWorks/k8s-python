2914efc7f898ff78f9b11b163c6cbb37c81ae899
Ben Greene <BenJGreene@gmail.com>
2024-10-21 00:12:57 +0100

    First attempt at using adev

--- container/app.py
+++ container/app.py
@@ -1,6 +1,12 @@
 # This file enables support for adev during development. It is not required for the application to run.
 from k8spython.service import service_start
 
-if __name__ == '__main__':
-    with open("config.yaml", "rb") as config_file:
+def app():
+    print("Starting service")
+    with open("tests/test_data/config.yaml", "rb") as config_file:
         service_start(config_file)
+
+
+# if __name__ == '__main__':
+#     with open("tests/test_data/config.yaml", "rb") as config_file:
+#         service_start(config_file)
