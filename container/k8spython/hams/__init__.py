1efa13392a36142ecad7491d76174711b97eb58a
Ben Greene <BenJGreene@gmail.com>
2024-10-26 11:11:56 +0100

    Basic alive/ready

--- container/k8spython/hams/__init__.py
+++ container/k8spython/hams/__init__.py
@@ -8,18 +8,26 @@ async def hams_app_cleanup(app: web.Application):
     """
     Cleanup the service
     """
-    click.secho("HaMS: Starting", fg="green")
-    hams_app = web.Application()
-    runner = web.AppRunner(hams_app)
+
+    click.secho("HaMS: starting", fg="green")
+    runner = web.AppRunner(app['hams'])
     await runner.setup()
     site = web.TCPSite(runner, 'localhost', 8079)
 
-    app['hams'] = hams_app
     await site.start()
     yield
 
-    click.secho("HaMS: Cleaning up", fg='red')
+    click.secho("HaMS: cleaning up", fg='red')
     await runner.cleanup()
+
+
+class AliveView(web.View):
+    async def get(self):
+        return web.Response(text="I'm alive")
+
+class ReadyView(web.View):
+    async def get(self):
+        return web.Response(text="I'm ready")
 
 
 def hams_app_create(app: web.Application, config: HamsConfig) -> web.Application:
@@ -27,9 +35,14 @@ def hams_app_create(app: web.Application, config: HamsConfig) -> web.Application
     Create the service with the given configuration file
     """
 
+    app['hams'] = web.Application()
+
+    app['hams'].router.add_get('/hams/alive', AliveView)
+    app['hams'].router.add_get('/hams/ready', ReadyView)
+
     app.cleanup_ctx.append(hams_app_cleanup)
 
     # https://docs.aiohttp.org/en/v3.8.4/web_advanced.html#cleanup-context
-
+    click.secho(f"HaMS: created", fg="green")
 
     return app
