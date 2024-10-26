ea24dbe806525a22547915b2ca73e12ab467d8e5
Ben Greene <BenJGreene@gmail.com>
2024-10-26 15:40:31 +0100

    Create Hams class

--- container/k8spython/hams/__init__.py
+++ container/k8spython/hams/__init__.py
@@ -10,7 +10,7 @@ async def hams_app_cleanup(app: web.Application):
     """
 
     click.secho("HaMS: starting", fg="green")
-    runner = web.AppRunner(app['hams'])
+    runner = web.AppRunner(app['hams'].hams_app)
     await runner.setup()
     site = web.TCPSite(runner, 'localhost', 8079)
 
@@ -23,11 +23,26 @@ async def hams_app_cleanup(app: web.Application):
 
 class AliveView(web.View):
     async def get(self):
-        return web.Response(text="I'm alive")
+
+        hams = self.request.app['hams']
+        print(hams.app['config'])
+        # print(dir(self.request.app['']))
+        alive = {
+            "alive": True
+        }
+        return web.json_response(alive)
+
 
 class ReadyView(web.View):
     async def get(self):
         return web.Response(text="I'm ready")
+
+
+class Hams:
+    def __init__(self, hams_app: web.Application, app: web.Application, config: HamsConfig):
+        self.app = app
+        self.hams_app = hams_app
+        self.config = config
 
 
 def hams_app_create(app: web.Application, config: HamsConfig) -> web.Application:
@@ -35,10 +50,15 @@ def hams_app_create(app: web.Application, config: HamsConfig) -> web.Application
     Create the service with the given configuration file
     """
 
-    app['hams'] = web.Application()
+    hams_app = web.Application()
+    hams = Hams(hams_app, app, config)
+    # Provide a ref back to app from HaMS
+    hams_app['hams'] = hams
+    app['hams'] = hams
 
-    app['hams'].router.add_get('/hams/alive', AliveView)
-    app['hams'].router.add_get('/hams/ready', ReadyView)
+
+    app['hams'].hams_app.router.add_get('/hams/alive', AliveView)
+    app['hams'].hams_app.router.add_get('/hams/ready', ReadyView)
 
     app.cleanup_ctx.append(hams_app_cleanup)
 
